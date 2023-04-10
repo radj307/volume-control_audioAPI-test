5744d90cd33a33764ca2dd8b5c197f45de3cf784
radj307 <radj307@gmail.com>
2023-04-10 16:08:01 -0400

    Make AudioDeviceManager.Devices list read-only & add XAML comments

--- Audio/AudioDeviceManager.cs
+++ Audio/AudioDeviceManager.cs
@@ -4,14 +4,14 @@ using CoreAudio;
 namespace Audio
 {
     /// <summary>
-    /// Manages a list of active audio devices.
+    /// Manages a list of <see cref="AudioDevice"/> instances.
     /// </summary>
     public sealed class AudioDeviceManager
     {
         #region Constructor
         public AudioDeviceManager(DataFlow deviceDataFlow)
         {
-            Devices = new();
+            _devices = new();
             DeviceDataFlow = deviceDataFlow;
 
             _deviceEnumerator = new MMDeviceEnumerator(_eventContext);
@@ -48,7 +48,87 @@ namespace Audio
         private void NotifyDeviceStateChanged(AudioDevice audioDevice) => DeviceStateChanged?.Invoke(this, audioDevice);
         #endregion Events
 
-        #region EventHandlers (_deviceNotificationClient)
+        #region Fields
+        private readonly Guid _eventContext = new();
+        private readonly MMDeviceEnumerator _deviceEnumerator;
+        private readonly MMNotificationClient _deviceNotificationClient;
+        #endregion Fields
+
+        #region Properties
+        private static VolumeControl.Log.LogWriter Log => VolumeControl.Log.FLog.Log;
+        /// <summary>
+        /// Gets the list of <see cref="AudioDevice"/> instances.
+        /// </summary>
+        public IReadOnlyList<AudioDevice> Devices => _devices;
+        /// <summary>
+        /// The underlying <see cref="List{T}"/> for the <see cref="Devices"/> property.
+        /// </summary>
+        private readonly List<AudioDevice> _devices;
+        /// <summary>
+        /// Gets the <see cref="DataFlow"/> type of the audio devices managed by this <see cref="AudioDeviceManager"/> instance.
+        /// </summary>
+        public DataFlow DeviceDataFlow { get; }
+        #endregion Properties
+
+        #region Methods
+        #region Methods (FindDevice)
+        /// <summary>
+        /// Gets the <see cref="AudioDevice"/> instance associated with the given <paramref name="deviceID"/> string. (<see cref="MMDevice.ID"/>)
+        /// </summary>
+        /// <param name="deviceID">The ID of the target device.</param>
+        /// <param name="comparisonType">The <see cref="StringComparison"/> type to use when comparing ID strings.</param>
+        /// <returns>The <see cref="AudioDevice"/> associated with the given <paramref name="deviceID"/> if found; otherwise <see langword="null"/>.</returns>
+        public AudioDevice? FindDeviceByID(string deviceID, StringComparison comparisonType = StringComparison.Ordinal)
+            => Devices.FirstOrDefault(dev => dev.ID.Equals(deviceID, comparisonType));
+        /// <summary>
+        /// Gets the <see cref="AudioDevice"/> instance associated with the given <paramref name="mmDevice"/>.
+        /// </summary>
+        /// <param name="mmDevice">The <see cref="MMDevice"/> instance associated with the target device.</param>
+        /// <returns>The <see cref="AudioDevice"/> associated with the given <paramref name="mmDevice"/> if found; otherwise <see langword="null"/>.</returns>
+        public AudioDevice? FindDeviceByMMDevice(MMDevice mmDevice)
+            => FindDeviceByID(mmDevice.ID);
+        #endregion Methods (FindDevice)
+
+        /// <summary>
+        /// Creates a new <see cref="AudioDevice"/> from the given <paramref name="mmDevice"/> and adds it to the <see cref="Devices"/> list, and triggers the <see cref="DeviceAddedToList"/> event.
+        /// </summary>
+        /// <param name="mmDevice">The <see cref="MMDevice"/> instance to create a new <see cref="AudioDevice"/> from.</param>
+        /// <returns>The newly-created <see cref="AudioDevice"/> instance if successful; otherwise <see langword="null"/> if <paramref name="mmDevice"/> is already associated with another device.</returns>
+        private AudioDevice? CreateAndAddDeviceIfUnique(MMDevice mmDevice)
+        {
+            if (FindDeviceByMMDevice(mmDevice) is null)
+            {
+                var audioDevice = new AudioDevice(mmDevice);
+
+                _devices.Add(audioDevice);
+
+                NotifyDeviceAddedToList(audioDevice);
+
+                return audioDevice;
+            }
+            else return null;
+        }
+        /// <summary>
+        /// Gets the default device for the specified <paramref name="deviceRole"/>.
+        /// </summary>
+        /// <param name="deviceRole">The <see cref="Role"/> of the target device.</param>
+        /// <returns>The default <see cref="AudioDevice"/> instance if one was found; otherwise <see langword="null"/>.</returns>
+        public AudioDevice? GetDefaultDevice(Role deviceRole)
+        {
+            try
+            {
+                var defaultEndpointMMDevice = _deviceEnumerator.GetDefaultAudioEndpoint(DeviceDataFlow, deviceRole);
+
+                return FindDeviceByID(defaultEndpointMMDevice.ID);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Getting default audio device with role '{deviceRole:G}' failed because an exception was thrown:", ex);
+                return null;
+            }
+        }
+
+        #region Methods (_deviceNotificationClient EventHandlers)
         private void DeviceNotificationClient_DeviceStateChanged(object? sender, DeviceStateChangedEventArgs e)
         {
             if (!e.TryGetDevice(out MMDevice? mmDevice) || mmDevice is null) return;
@@ -73,7 +153,7 @@ namespace Audio
                 {
                     NotifyDeviceStateChanged(existingAudioDevice);
 
-                    Devices.Remove(existingAudioDevice);
+                    _devices.Remove(existingAudioDevice);
 
                     Log.Debug($"{nameof(AudioDevice)} '{existingAudioDevice.Name}' state changed to {mmDevice.State:G}; removed it from the list.");
 
@@ -112,7 +192,7 @@ namespace Audio
         {
             if (FindDeviceByID(e.DeviceId) is AudioDevice audioDevice)
             {
-                Devices.Remove(audioDevice);
+                _devices.Remove(audioDevice);
 
                 Log.Debug($"{nameof(AudioDevice)} '{audioDevice.Name}' was removed from the system; it was removed from the list.");
 
@@ -121,83 +201,7 @@ namespace Audio
                 audioDevice.Dispose();
             }
         }
-        #endregion EventHandlers (_deviceNotificationClient)
-
-        #region Fields
-        private readonly Guid _eventContext = new();
-        private readonly MMDeviceEnumerator _deviceEnumerator;
-        private readonly MMNotificationClient _deviceNotificationClient;
-        #endregion Fields
-
-        #region Properties
-        private static VolumeControl.Log.LogWriter Log => VolumeControl.Log.FLog.Log;
-        /// <summary>
-        /// Gets the list of <see cref="AudioDevice"/> instances.
-        /// </summary>
-        public List<AudioDevice> Devices { get; }
-        /// <summary>
-        /// Gets the <see cref="DataFlow"/> type of the audio devices managed by this <see cref="AudioDeviceManager"/> instance.
-        /// </summary>
-        public DataFlow DeviceDataFlow { get; }
-        #endregion Properties
-
-        #region Methods
-        #region Methods (FindDevice)
-        /// <summary>
-        /// Gets the <see cref="AudioDevice"/> instance associated with the given <paramref name="deviceID"/> string. (<see cref="MMDevice.ID"/>)
-        /// </summary>
-        /// <param name="deviceID">The ID of the target device.</param>
-        /// <param name="comparisonType">The <see cref="StringComparison"/> type to use when comparing ID strings.</param>
-        /// <returns>The <see cref="AudioDevice"/> associated with the given <paramref name="deviceID"/> if found; otherwise <see langword="null"/>.</returns>
-        public AudioDevice? FindDeviceByID(string deviceID, StringComparison comparisonType = StringComparison.Ordinal)
-            => Devices.FirstOrDefault(dev => dev.ID.Equals(deviceID, comparisonType));
-        /// <summary>
-        /// Gets the <see cref="AudioDevice"/> instance associated with the given <paramref name="mmDevice"/>.
-        /// </summary>
-        /// <param name="mmDevice">The <see cref="MMDevice"/> instance associated with the target device.</param>
-        /// <returns>The <see cref="AudioDevice"/> associated with the given <paramref name="mmDevice"/> if found; otherwise <see langword="null"/>.</returns>
-        public AudioDevice? FindDeviceByMMDevice(MMDevice mmDevice)
-            => FindDeviceByID(mmDevice.ID);
-        #endregion Methods (FindDevice)
-
-        /// <summary>
-        /// Creates a new <see cref="AudioDevice"/> from the given <paramref name="mmDevice"/> and adds it to the <see cref="Devices"/> list, and triggers the <see cref="DeviceAddedToList"/> event.
-        /// </summary>
-        /// <param name="mmDevice">The <see cref="MMDevice"/> instance to create a new <see cref="AudioDevice"/> from.</param>
-        /// <returns>The newly-created <see cref="AudioDevice"/> instance if successful; otherwise <see langword="null"/> if <paramref name="mmDevice"/> is already associated with another device.</returns>
-        private AudioDevice? CreateAndAddDeviceIfUnique(MMDevice mmDevice)
-        {
-            if (FindDeviceByMMDevice(mmDevice) is null)
-            {
-                var audioDevice = new AudioDevice(mmDevice);
-
-                Devices.Add(audioDevice);
-
-                NotifyDeviceAddedToList(audioDevice);
-
-                return audioDevice;
-            }
-            else return null;
-        }
-        /// <summary>
-        /// Gets the default device for the specified <paramref name="deviceRole"/>.
-        /// </summary>
-        /// <param name="deviceRole">The <see cref="Role"/> of the target device.</param>
-        /// <returns>The default <see cref="AudioDevice"/> instance if one was found; otherwise <see langword="null"/>.</returns>
-        public AudioDevice? GetDefaultDevice(Role deviceRole)
-        {
-            try
-            {
-                var defaultEndpointMMDevice = _deviceEnumerator.GetDefaultAudioEndpoint(DeviceDataFlow, deviceRole);
-
-                return FindDeviceByID(defaultEndpointMMDevice.ID);
-            }
-            catch (Exception ex)
-            {
-                Log.Error($"Getting default audio device with role '{deviceRole:G}' failed because an exception was thrown:", ex);
-                return null;
-            }
-        }
+        #endregion Methods (_deviceNotificationClient EventHandlers)
         #endregion Methods
     }
 }
