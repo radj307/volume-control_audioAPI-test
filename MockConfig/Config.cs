56040ddd9a61de7c8863ee4f83a9eb6872e74356
radj307 <radj307@gmail.com>
2023-10-07 10:15:58 -0400

    create a MockConfig to emulate having a config at runtime

---
+++ MockConfig/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using WPF;
+
+namespace MockConfig
+{
+    public class Config : INotifyPropertyChanged
+    {
+        #region Mock
+        static Config()
+        {
+            Default = new();
+        }
+        public static Config Default { get; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion Mock
+
+        public string TargetDeviceID { get; set; } = string.Empty;
+        public TargetInfo TargetSession { get; set; } = TargetInfo.Empty;
+        public TargetInfo[] TargetSessions { get; set; } = Array.Empty<TargetInfo>();
+        public ObservableImmutableList<string> HiddenSessionProcessNames { get; set; } = new();
+    }
+}
